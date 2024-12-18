d0da31dccdc1cbe31196eb093e55861a5129655d
stern-sigma <trainee.samuel.stern@sigmalabs.co.uk>
2024-12-18 13:01:47 +0000

    Actually uploads airports

--- week-8/load_airports.py
+++ week-8/load_airports.py
@@ -1,3 +1,6 @@
+# %% [markdown]
+# Imports
+
 # %%
 import pandas as pd
 import redshift_connector
@@ -6,6 +9,9 @@ from os import environ
 from dotenv import load_dotenv
 load_dotenv()
 
+
+# %% [markdown]
+# Import and load new airports data
 
 # %%
 all_airports = pd.read_csv('https://www.fly.faa.gov/rmt/data_file/locid_db.csv',
@@ -16,6 +22,9 @@ all_airports['city'] = all_airports['Location'].map(lambda x: x.split('.')[0])
 all_airports['state'] = all_airports['Location'].map(lambda x: x.split('.')[1].strip())
 all_airports = all_airports.drop(["Facility", "Location"], axis=1)
 all_airports
+
+# %% [markdown]
+# Create redshift connection
 
 # %%
 import redshift_connector
@@ -29,6 +38,9 @@ conn = redshift_connector.connect(
     port = environ['DB_PORT'],
     host = environ['DB_HOST']
 )
+
+# %% [markdown]
+# Load a list of known airports
 
 # %%
 cur = conn.cursor()
@@ -45,9 +57,15 @@ known_airports = {x[0] for x in cur.fetchall()}
 cur.close()
 known_airports
 
+# %% [markdown]
+# Remove known airports from the data to upload
+
 # %%
 airports_to_add = all_airports.drop(all_airports[all_airports['airport_name'].isin(known_airports)].index)
 airports_to_add
+
+# %% [markdown]
+# Load a set of known cities
 
 # %%
 cur = conn.cursor()
@@ -63,6 +81,9 @@ FROM
 known_cities = {x[0] for x in cur.fetchall()}
 cur.close()
 known_cities
+
+# %% [markdown]
+# Convert state codes to state ids
 
 # %%
 cur = conn.cursor()
@@ -80,6 +101,9 @@ state_dict = {x[0]: x[1] for x in cur.fetchall()}
 cur.close()
 airports_to_add['state'] = airports_to_add['state'].map(state_dict)
 airports_to_add
+
+# %% [markdown]
+# Generate complete data for new cities
 
 # %%
 cities_to_add = airports_to_add[['city', 'state']]
@@ -112,6 +136,9 @@ cities_to_add = [(next(curr_ind), x[0], x[1]) for x in cities_to_add.itertuples(
 cities_to_add
 
 
+# %% [markdown]
+# Upload new cities
+
 # %%
 cur = conn.cursor()
 cur.executemany(
@@ -125,6 +152,9 @@ VALUES
 )
 conn.commit()
 cur.close()
+
+# %% [markdown]
+# Create a cities id dictionary for mapping city names
 
 # %%
 cur = conn.cursor()
@@ -142,10 +172,58 @@ cities_dict = {x[0]:x[1] for x in cur.fetchall()}
 cur.close()
 cities_dict
 
+# %% [markdown]
+# Convert city names to city ids and drop redundant state column
+
 # %%
-all_airports['city'] = all_airports['city'].map(cities_dict)
-all_airports = all_airports.drop('state', axis=1)
-all_airports
+airports_to_add['city'] = airports_to_add['city'].map(cities_dict)
+airports_to_add = airports_to_add.drop('state', axis=1)
+airports_to_add
+
+# %% [markdown]
+# Filter out known airports from data to upload
+
+# %%
+cur = conn.cursor()
+cur.execute(
+"""
+SELECT 
+    airport_id
+FROM
+    airport
+;
+"""
+)
+known_airports = {x[0] for x in cur.fetchall()}
+airports_to_drop = airports_to_add[airports_to_add["LocID"].isin(known_airports)].index
+airports_to_add = airports_to_add.drop(airports_to_drop)
+cur.close()
+airports_to_add
+
+# %% [markdown]
+# Upload new airports
+
+# %%
+cur = conn.cursor()
+cur.execute('SELECT MAX(airport_id) FROM airport;')
+airport_index = Identity(cur.fetchone()[0])
+new_airports = [(next(airport_index), x[0], x[1], x[2]) 
+                for x in airports_to_add.itertuples(index=False)
+                if len(x[0]) <= 5]
+cur.executemany(
+""" 
+INSERT INTO 
+    airport
+    (airport_id, airport_code, airport_name, city_id)
+VALUES
+    (%s, %s, %s, %s)
+;
+""", param_sets=new_airports
+)
+cur.close()
+
+# %% [markdown]
+# Teardown
 
 # %%
 conn.close()
